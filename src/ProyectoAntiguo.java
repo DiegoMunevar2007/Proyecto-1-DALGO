684ae98b5cf6c75d81f289ef93dcbb812c8591d0
Diego Alejandro Munévar Pérez <d.munevar@uniandes.edu.co>
2025-03-12 21:22:21 -0500

    Optimizao'

--- src/ProyectoAntiguo.java
+++ src/ProyectoAntiguo.java
@@ -13,21 +13,39 @@
          for (int i = 0; i <= numeroJugadores; i++) {
              Arrays.fill(dp[i], Integer.MAX_VALUE);
          }
+         if (numeroIntercambios>= (numeroJugadores*(pesos.length*numeroJugadores))){
+            ArrayList<Integer> lista = new ArrayList<Integer>();
+            for (int i = 0; i < pesos.length; i++) {
+                lista.add(pesos[i]);
+            }
+            Collections.sort(lista);
+            int suma = 0;
+            for (int i = 0; i < numeroJugadores; i++) {
+                suma += lista.get(i);
+            }
+            Long tiempoFinal = System.currentTimeMillis();
+            return suma;
+         }
          dp[0][0] = 0;
          for (int i = 0; i < pesos.length; i++) { // recorrer los jugadores en orden
              // Recorremos el número de jugadores ya seleccionados en orden inverso:
              for (int k = Math.min(numeroJugadores - 1, i); k >= 0; k--) { // Tomamos el mínimo entre el número de jugadores y el índice actual.
-                 int costo = i - k; // El costo de seleccionar al jugador i como el (k+1)-ésimo.
-                 if (costo > numeroIntercambios) {
-                     continue;
-                 }
-                 for (int w = 0; w <= numeroIntercambios - costo; w++) {
-                     if (dp[k][w] != Integer.MAX_VALUE) {
-                         int nuevoSwap = w + costo;
-                         int nuevoCosto = dp[k][w] + pesos[i];
-                         dp[k + 1][nuevoSwap] = Math.min(dp[k + 1][nuevoSwap], nuevoCosto);
-                     }
-                 }
+                // Esto significa que no podemos seleccionar más jugadores que los que hemos visto.
+                // Si el número de jugadores que hemos visto es menor que el número de jugadores que queremos seleccionar, no podemos seleccionar a nadie.
+                if ((pesos.length - i) >= (numeroJugadores - k) ) {
+                    
+                    int costo = i - k; // El costo de seleccionar al jugador i como el (k+1)-ésimo.
+                    if (costo <= numeroIntercambios) {
+                    
+                        for (int w = 0; w <= numeroIntercambios - costo; w++) {
+                            if (dp[k][w] != Integer.MAX_VALUE) {
+                                int nuevoSwap = w + costo;
+                                int nuevoCosto = dp[k][w] + pesos[i];
+                                dp[k + 1][nuevoSwap] = Math.min(dp[k + 1][nuevoSwap], nuevoCosto);
+                            }
+                        }
+                    }
+                }
              }
          }
          int minimo = Integer.MAX_VALUE;
